ffc0cf48e3cd6619dec50f271b7af43d219b578d
Divya1561 <68775489+Divya1561@users.noreply.github.com>
2021-04-02 21:52:28 +0530

    Add files via upload

---
+++ WebAPIProject_CE139_CE147/OLX/Controllers/ItemController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OLX.Models;
+
+
+
+
+
+namespace OLX.Controllers
+{
+    public class ItemController : ApiController
+    {
+        DatabaseContext db = new DatabaseContext();
+
+        public IEnumerable<Item> GetItems()
+        {
+            return db.Items.ToList();
+        }
+
+        public Item GetItems(int Id)
+        {
+            return db.Items.Find(Id);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage AddItem(Item model)
+        {
+            try
+            {
+                db.Items.Add(model);
+                db.SaveChanges();
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.Created);
+                return response;
+            }
+            catch(Exception ex)
+            {
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                return response;
+            }
+        }
+
+        [HttpPut]
+        public HttpResponseMessage UpdateItem(int id, Item model)
+        {
+            try
+            {
+                if(id==model.Id)
+                {
+                    db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+                    return response;
+                }
+                else
+                {
+                    HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.NotModified);
+                    return response;
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                return response;
+            }
+        }
+
+        public HttpResponseMessage DeleteItem(int id)
+        {
+            Item item = db.Items.Find(id);
+            if(item != null)
+            {
+                db.Items.Remove(item);
+                db.SaveChanges();
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+                return response;
+            }
+            else
+            {
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.NotFound);
+                return response;
+            }
+        }
+    }
+}
---
+++ WebAPIProject_CE139_CE147/OLX/Models/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+
+namespace OLX.Models
+{
+    public class DatabaseContext: DbContext
+    {
+        public DatabaseContext() : base("DefaultConnection")
+        {
+            this.Configuration.ProxyCreationEnabled = false;
+
+
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
